0f0a3ffe3c6f141d4e59e888608284a07b2d395a
mevipinmaurya <mevipin2212001@gmail.com>
2024-04-20 20:11:14 +0530

    Phase 1 part a completed

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require("express");
 const app = express()
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
-
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const path = require("path");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.engine('ejs', ejsMate);
 
+app.use(express.static(path.join(__dirname, "/public")))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride("_method"));
 
