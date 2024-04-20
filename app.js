c1377d8ad22b71c2c2883adc886af92ddfb63ea2
mevipinmaurya <mevipin2212001@gmail.com>
2024-04-21 00:13:45 +0530

    Phase 1 part c completed, new package (joi) used for server side validation

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+
+// For server side validation (joi)
+const listingSchema = require("./schema.js");
 
 const path = require("path");
 
@@ -42,11 +47,22 @@ async function main() {
 // })
 
 
+const validateListing = (req, res, next) => {
+    let result = listingSchema.validate(req.body);
+    console.log(result);
+    if (result.error) {
+        throw new ExpressError(400, result.error);
+    } else {
+        next();
+    }
+}
+
+
 // Index route
-app.get("/listings", async (req, res) => {
+app.get("/listings", wrapAsync(async (req, res) => {
     let allListings = await Listing.find();
     res.render("listings/index.ejs", { allListings });
-})
+}))
 
 // new route
 app.get("/listings/new", (req, res) => {
@@ -54,7 +70,7 @@ app.get("/listings/new", (req, res) => {
 })
 
 // Create route
-app.post("/listings", async (req, res) => {
+app.post("/listings", validateListing, wrapAsync(async (req, res, next) => {
     let { title: newTitle, description: newDesc, image: newImg, price: newprice, location: newLoc, country: newCount } = req.body;
     const newListing = new Listing({
         title: newTitle,
@@ -65,20 +81,41 @@ app.post("/listings", async (req, res) => {
         country: newCount
     })
 
+    // ***************** Schema Validation ********************
+    // if(!newListing.title){
+    //     throw new ExpressError(400, "Title is missing");
+    // }
+
+    // if(!newListing.description){
+    //     throw new ExpressError(400, "Description is missing");
+    // }
+
+    // if(!newListing.location){
+    //     throw new ExpressError(400, "Location is missing");
+    // }
+
+    // if(!newListing.country){
+    //     throw new ExpressError(400, "Country is missing");
+    // }
+
+    // if(!newListing.price){
+    //     throw new ExpressError(400, "Price is missing");
+    // }
+
     await newListing.save();
     res.redirect("/listings");
-})
+}))
 
 // edit route
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
     let { id } = req.params;
     let listing = await Listing.findById(id);
     res.render("listings/edit.ejs", { listing });
-})
+}))
 
 // update route
-app.put("/listings/:id", async (req, res) => {
-    let {id} = req.params;
+app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {
+    let { id } = req.params;
     let { title: newTitle, description: newDes, price: newPrice, image: newImg, location: newLoc, country: newCount } = req.body;
     await Listing.findByIdAndUpdate(id, {
         title: newTitle,
@@ -89,26 +126,41 @@ app.put("/listings/:id", async (req, res) => {
         country: newCount,
     })
     res.redirect(`/listings/${id}`);
-})
+}))
 
 // Destroy route
-app.delete("/listings/:id", async (req, res)=>{
-    let {id} = req.params;
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
+    let { id } = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
-})
+}))
 
 
 // show route
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     let listing = await Listing.findById(id);
     res.render("listings/show.ejs", { listing });
-})
+}))
 
 
 app.get("/", (req, res) => {
     res.send("Building a major Project")
+})
+
+
+// Below code will match all route to the entered route if match found then response will be send otherwise page Not found error occur
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, "Page Not Found !!!"));
+})
+
+
+// Middleware (Error Handling)
+app.use((err, req, res, next) => {
+    let { statusCode = 500, message = "Something Went Wrong !!!" } = err;
+    // res.send("Something Went Wrong !!!");
+    // res.status(statusCode).send(message);
+    res.status(statusCode).render("error.ejs", { message });
 })
 
 app.listen(8080, () => {
